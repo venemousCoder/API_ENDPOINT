52f8082c8626fbecd67f2d340194f138ab4b6125
Ahmad Dalha <125165480+venemousCoder@users.noreply.github.com>
2024-07-03 02:35:49 +0100

    Update apiControllers.js
    few tweaks for more accuracy

--- controllers/apiControllers.js
+++ controllers/apiControllers.js
@@ -1,10 +1,28 @@
-const geoip = require("geoip-lite");
+const express = require('express');
+const fetch = require('node-fetch');
+const geoip = require('geoip-lite');
 
+const app = express();
+
+// Trust the proxy to get the real client IP
+app.set('trust proxy', true);
+
+function getClientIp(req) {
+    const xForwardedFor = req.headers['x-forwarded-for'];
+    if (xForwardedFor) {
+        // 'x-forwarded-for' may return a comma-separated list of IP addresses, take the first one
+        return xForwardedFor.split(',')[0];
+    }
+    // Fall back to req.connection.remoteAddress
+    return req.connection.remoteAddress;
+}
 function apiResponse(req, res) {
     const visitor_name = req.query.visitor_name;
-    const ipAddress = "103.86.96.100"; // req.ip;
+    const ipAddress = getClientIp(req);
     const geodata = geoip.lookup(ipAddress);
-
+    const lat = geodata.ll[0];
+    const lon = geodata.ll[1];
+    console.log(ipAddress, geodata);
     if (!geodata) {
         const error = {
             errorType: "server_error",
@@ -13,16 +31,16 @@ function apiResponse(req, res) {
         };
         return res.json(error);
     }
-
-    fetch(`http://api.weatherapi.com/v1/current.json?key=d8dbdedcd37248dea52204502240207&q=${geodata.city}`)
+      
+    fetch(`http://api.weatherapi.com/v1/current.json?key=${process.env.API_KEY}&q=${ipAddress}`)
         .then((response) => {
             return response.json();
         })
         .then((weatherData) => {
             const info = {
                 "client_ip": `${ipAddress}`,
-                "location": `${geodata.city}`,
-                "greeting": `Hi ${visitor_name}, It's ${weatherData.current.temp_c}°C in ${geodata.city} today!`
+                "location": `${weatherData.location.region || geodata.city}`,
+                "greeting": `Hi ${visitor_name}, It's ${weatherData.current.temp_c}°C in ${weatherData.location.region || geodata.city} today!`
             };
             console.log(info);
             return res.json(info);
